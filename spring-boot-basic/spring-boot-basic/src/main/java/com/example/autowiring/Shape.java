6d3158bba3088acd243900ab22af46c13f8f7be3
hareeshakona <134270721+hareeshakona@users.noreply.github.com>
2023-06-06 11:48:26 +0530

    Delete spring-boot-basic directory

--- spring-boot-basic/spring-boot-basic/src/main/java/com/example/autowiring/Shape.java
+++
@@ -1,6 +0,0 @@
-package com.example.autowiring;
-
-public interface Shape {
-
-	void area(int x, int y);
-}
--- spring-boot-basic/src/main/java/com/example/javabased/ISports.java
+++
@@ -1,8 +0,0 @@
-package com.example.javabased;
-
-import java.util.List;
-
-public interface ISports {
-
-	List<String> playMatches();
-}
